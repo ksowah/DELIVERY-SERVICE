29ee8c609b6573b4b68c4bbc1cf99cb08956849a
ksowah <sowahkelvin640@gmail.com>
2022-07-13 23:44:58 +0000

    edit tw config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+            "./components/**/*.{js,ts,jsx,tsx}",
+            "./screens/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
