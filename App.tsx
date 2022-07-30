90f23fc9de7f4d8909a170bba4e0f3308f0182bc
ksowah <sowahkelvin640@gmail.com>
2022-07-30 18:19:24 +0000

    add new stack

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
+import Restaurant from "./screens/Restaurant";
 
-const Stack = createNativeStackNavigator()
-
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-    <NavigationContainer>
-		<TailwindProvider >
-			<Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}/>
-      </Stack.Navigator>
-		</TailwindProvider>
-    </NavigationContainer>
+		<NavigationContainer>
+			<TailwindProvider>
+				<Stack.Navigator>
+					<Stack.Screen name="Home" component={Home} />
+					<Stack.Screen name="Restaurant" component={Restaurant} />
+				</Stack.Navigator>
+			</TailwindProvider>
+		</NavigationContainer>
 	);
 }
