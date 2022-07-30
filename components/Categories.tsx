115ab41d3a49804b99af8a9915abd6ba5b7034ad
ksowah <sowahkelvin640@gmail.com>
2022-07-30 18:20:40 +0000

    pull categories from data

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,19 +1,25 @@
 import { ScrollView } from 'react-native'
 import React from 'react'
 import CategoryCard from './CategoryCard'
+import { categories } from '../data'
 
-const Categories = () => {
+
+interface Props {
+  image: String,
+  title: String
+}
+
+const Categories = ({image, title}: Props) => {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}
       contentContainerStyle={{paddingHorizontal: 15, paddingTop: 10}}
     >
-      <CategoryCard image="https://links.papareact.com/gn7" title="testing"/>
-      <CategoryCard image="https://links.papareact.com/gn7" title="testing"/>
-      <CategoryCard image="https://links.papareact.com/gn7" title="testing"/>
-      <CategoryCard image="https://links.papareact.com/gn7" title="testing"/>
-      <CategoryCard image="https://links.papareact.com/gn7" title="testing"/>
-      <CategoryCard image="https://links.papareact.com/gn7" title="testing"/>
-      <CategoryCard image="https://links.papareact.com/gn7" title="testing"/>
+       {
+            categories.map( (cat, idx) => (
+              <CategoryCard key={idx} image={cat.image} title={cat.title} />
+            ) )
+        }
+      
     </ScrollView>
   )
 }
