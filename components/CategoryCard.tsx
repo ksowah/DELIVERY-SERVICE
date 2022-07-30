b8e2a3d0acadf8453343017445e4a6ce1565f4d9
ksowah <sowahkelvin640@gmail.com>
2022-07-30 18:22:01 +0000

    change image source

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -12,7 +12,7 @@ const CategoryCard = ({image, title}: Props) => {
 
   return (
     <TouchableOpacity className="relative mr-2">
-      <Image source={{uri: image || "https://links.papareact.com/wru"}}
+      <Image source={image || "https://links.papareact.com/wru"}
         className="w-20 h-20 rounded"
       />
       <Text className="absolute bottom-0 left-1 text-white font-bold">{title}</Text>
