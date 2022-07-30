14ddc55de7cada7cb46fe3af537acbbf20995c1b
ksowah <sowahkelvin640@gmail.com>
2022-07-30 18:22:43 +0000

    change image source

--- components/RestaurantCard.tsx
+++ components/RestaurantCard.tsx
@@ -19,14 +19,14 @@ interface Props {
 const RestaurantCard = ({ id, image, title, rating, genre, address, short_description, dishes, long, lat }: Props) => {
   return (
     <TouchableOpacity className="bg-white mr-3 shadow">
-      <Image source={{uri: image}}
+      <Image source={image}
         className="h-36 w-64 rounded-sm"
       />
 
       <View className="px-3 pb-4">
         <Text className="font-bold text-lg pt-2">{title}</Text>
         <View className="flex-row items-center space-x-1">
-        <AntDesign name="star" size={22} color="green" className="opacity-50" />
+        <AntDesign name="star" size={22} color="#00ccbb" className="opacity-50" />
         <Text className="text-xs text-gray-500"> 
             <Text className="text-green-500">{rating}</Text> . {genre}
         </Text>
