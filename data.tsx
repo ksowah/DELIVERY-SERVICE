0d6f2e488ecf15adb6038bdd4de792af9763a69a
ksowah <sowahkelvin640@gmail.com>
2022-07-30 18:19:51 +0000

    add data

---
+++ data.tsx
@@ -0,0 +1,174 @@
+export const featuredCategories = [
+    {
+        title: "Offers near you!",
+        description: "Why not support your local restaurant tonight",
+        id: 1
+    },
+    {
+        title: "Fetured!",
+        description: "Payed placements from our partners",
+        id: 2
+    },
+    {
+        title: "Tasty Discounts!",
+        description: "Everyone's been enjoying these juicy discounts",
+        id: 3
+    },
+]
+
+export const featuredMenuCategories = [
+    {
+        name: "Ocean View",
+        description: "Get your delicious and hot meals at the Ocean View restaurant. Have a good look at the beautiful ocean",
+        image: require("./assets/res-16.jpg"),
+        lat: 67.46477,
+        lon: 114.78458,
+        address: "GR8 Stoke Street",
+        rating: 5,
+        id: 1,
+        
+    },
+    {
+        name: "The Palestine",
+        description: "Get your delicious and hot meals at the Palestine restaurant. ",
+        image: require("./assets/res-5.jpg"),
+        lat: 41.06321,
+        lon: -103.01914,
+        address: "AP6 Sanity Street",
+        rating: 4,
+        id: 1,
+    },
+    {
+        name: "Break Fast ",
+        description: "Take the edge of with the best breakfast around",
+        image: require("./assets/res-4.jpg"),
+        lat: -75.81226,
+        lon: 28.96597,
+        address: "PR25 Ocksland Street",
+        rating: 5,
+        id: 1,
+    },
+    {
+        name: "Fisher",
+        description: "One stop shop for for fresh fried fishes",
+        image: require("./assets/restaurant-2.jpg"),
+        lat: 30.27721,
+        lon: 50.69273,
+        address: "AR12 Jake Street",
+        rating: 5,
+        id: 1,
+    },
+
+    // second 
+    {
+        name: "The Caribbean",
+        description: "Get your delicious and hot meals at the The Caribbean.",
+        image: require("./assets/restaurant-3.jpg"),
+        lat: 27.53843,
+        lon: 103.93669,
+        address: "CR8 Pirates Street",
+        rating: 4,
+        id: 2,
+    },
+    {
+        name: "Night Market",
+        description: "Get your delicious and hot meals at the Night Market.",
+        image: require("./assets/res-1.jpg"),
+        lat: 45.08343,
+        lon: 35.3256,
+        address: "RA4 Golden Street",
+        rating: 3,
+        id: 2,
+    },
+    {
+        name: "Grimm",
+        description: "Evenings are great at the grim. pay us a visit",
+        image: require("./assets/res-8.jpg"),
+        lat: 45.08343,
+        lon: 35.3256,
+        address: "GR4 Grimm Street",
+        rating: 4,
+        id: 2,
+    },
+
+    // third
+    {
+        name: "Shelve Shift",
+        description: "They always come back on the first try. This is your first time here? We look forward to a fruitful friendship with our delicious meals",
+        image: require("./assets/res-6.jpg"),
+        lat: 45.08343,
+        lon: 35.3256,
+        address: "CP8 Atlantic Street",
+        rating: 5,
+        id: 2,
+    },
+    {
+        name: "Light Haven",
+        description: "Bring your loved ones to the light haven, you'll wish you did earlier",
+        image: require("./assets/res-9.jpg"),
+        lat: 45.08343,
+        lon: 35.3256,
+        address: "PR3 Stoke Street",
+        rating: 4,
+        id: 3,
+    },
+    {
+        name: "Mighty Stake",
+        description: "Stakes are greate, but have you tried the ones at mighty stakes? It's about time you joined the club",
+        image: require("./assets/res-10.jpg"),
+        lat: 45.08343,
+        lon: 35.3256,
+        address: "MS6 Mass Street",
+        rating: 5,
+        id: 3,
+    },
+    {
+        name: "Blue Fill",
+        description: "Get your delicious and hot meals at the Blue Fill.",
+        image: require("./assets/res-11.jpg"),
+        lat: 45.08343,
+        lon: 35.3256,
+        address: "PC8 Procto Street",
+        rating: 4,
+        id: 3,
+    },
+    {
+        name: "The Gladiators",
+        description: "There's nothing like a good family re-union. The gladiators brings you close to loved ones.",
+        image: require("./assets/res-12.jpg"),
+        lat: 45.08343,
+        lon: 35.3256,
+        address: "BC5 Gladiators Street",
+        rating: 4,
+        id: 3,
+    },
+
+]
+
+
+export const categories = [
+    {
+        title: "Caribbean",
+        image: require("./assets/caribbean.jpg")
+    },
+    {
+        title: "Exotic",
+        image: require("./assets/exotic.jpg")
+    },
+    {
+        title: "Asian",
+        image: require("./assets/asian.jpg")
+    },
+    {
+        title: "Local",
+        image: require("./assets/cat-1.jpg")
+    },
+    {
+        title: "Breakfast",
+        image: require("./assets/fast.jpg")
+    },
+    {
+        title: "Chinese",
+        image: require("./assets/fish.jpg")
+    },
+]
